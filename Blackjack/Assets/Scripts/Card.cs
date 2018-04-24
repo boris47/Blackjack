e0ec98f1b7bc5cd285287189acbc55c468dae68c
RobertoLeogrande <boris47@hotmail.it>
2018-04-24 19:27:52 +0200

    Initial setup

---
+++ Blackjack/Assets/Scripts/Card.cs
@@ -0,0 +1,44 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Card : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+
+	private void Awake()
+	{
+		
+	}
+
+	private void OnEnable()
+	{
+		
+	}
+
+	private void OnDisable()
+	{
+		
+	}
+
+	private void Start()
+	{
+		
+	}
+
+
+	void IBeginDragHandler.OnBeginDrag( PointerEventData eventData )
+	{
+		
+	}
+
+	void IDragHandler.OnDrag( PointerEventData eventData )
+	{
+		
+	}
+
+	void IEndDragHandler.OnEndDrag( PointerEventData eventData )
+	{
+		
+	}
+
+}
---
+++ Blackjack/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿
+
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private void Awake()
+	{
+		
+	}
+
+	private void Start()
+	{
+		
+	}
+
+
+	private void Update()
+	{
+		
+	}
+
+}
